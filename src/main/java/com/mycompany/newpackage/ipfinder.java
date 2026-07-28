a5850c4a0932e1a96285d49f75e0967dc364b984
Harsh Kohli <22bcs10115@cuchd.in>
2026-07-28 14:10:08 +0530

    Update ipfinder.java

--- src/main/java/com/mycompany/newpackage/ipfinder.java
+++ src/main/java/com/mycompany/newpackage/ipfinder.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.mycompany.newpackage;
 import java.net.*;
 import javax.swing.JOptionPane;
@@ -15,7 +11,7 @@ public class ipfinder extends javax.swing.JFrame {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+  
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -59,9 +55,9 @@ public class ipfinder extends javax.swing.JFrame {
 
         pack();
         setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
       String url=input.getText();
       try{
           InetAddress ia=InetAddress.getByName(url);
@@ -75,16 +71,14 @@ public class ipfinder extends javax.swing.JFrame {
       }
       
         
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }
 
     public static void main(String args[]) {
        
         java.awt.EventQueue.invokeLater(() -> new ipfinder().setVisible(true));
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField input;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    // End of variables declaration//GEN-END:variables
+  
 }
